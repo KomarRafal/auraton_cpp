408047ca8e13f792843a082286adc67cd1a25ee2
Rafal <komarro@gmail.com>
2021-04-16 10:21:10 +0200

    specific_source parser added

---
+++ lib/auralib/parsers/specific_source.hpp
@@ -0,0 +1,33 @@
+/*
+ * specific_source.hpp
+ */
+
+#ifndef SPECIFIC_SOURCE_HPP_
+#define SPECIFIC_SOURCE_HPP_
+
+#include <string>
+#include "source.hpp"
+
+namespace aura
+{
+
+namespace parser
+{
+
+class specific_source : public source {
+public:
+	explicit specific_source(std::string specific_source);
+	virtual bool is_value_ok(const std::string_view& value) const;
+
+	static const std::string COMMAND;
+	static const std::string EVENT;
+
+private:
+	std::string source;
+};
+
+}
+
+}
+
+#endif
--- tests/parsers/specific_source_parser_ut.cpp
+++ tests/parsers/specific_source_parser_ut.cpp
@@ -1,9 +1,9 @@
 /*
- * specific_command_parser_ut.cpp
+ * specific_source_parser_ut.cpp
  */
 
 #include "gtest/gtest.h"
-#include "parsers/commands/specific_command.hpp"
+#include "parsers/specific_source.hpp"
 
 const std::string test_payload {
 	"Lorem ipsum dolor sit amet,\r\n"
@@ -11,39 +11,40 @@ const std::string test_payload {
 	"tempor incididunt ut labore et dolore magna aliqua.\r\n"
 };
 
-TEST(specific_command_parser_ut, specific_command_test_ok)
+TEST(specific_source_parser_ut, specific_source_parse_test_ok)
 {
-	const std::string command {
-		"SPECIFIC_COMMAND"
+	const std::string specific_source {
+		"SPECIFIC_SOURCE"
 	};
 	const std::string test_string {
-		"COMMAND:" +
-		command +
+		"SOURCE:" +
+		specific_source +
 		"\r\n" +
 		test_payload
 	};
 	std::string_view test_string_view(test_string);
-	aura::parser::commands::specific_command parser_ut(command);
+	aura::parser::specific_source parser_ut(specific_source);
 	const auto parse_result = parser_ut.parse(test_string_view);
 	EXPECT_TRUE(parse_result);
 	EXPECT_EQ(test_payload, static_cast<std::string>(test_string_view));
 }
 
-TEST(specific_command_parser_ut, specific_command_test_fail)
+TEST(specific_source_parser_ut, specific_source_parse_test_wrong_token)
 {
-	const std::string command {
-		"SPECIFIC_COMMAND"
+	const std::string specific_source {
+		"SPECIFIC_SOURCE"
 	};
-	const std::string wrong_command {
-		"WRONG_COMMAND"
+	const std::string wrong_source {
+		"WRONG_SOURCE"
 	};
 	const std::string test_string {
-		"COMMAND:" +
-		command +
-		"\r\n"
+		"SOOURCE:" +
+		wrong_source +
+		"\r\n" +
+		test_payload
 	};
 	std::string_view test_string_view(test_string);
-	aura::parser::commands::specific_command parser_ut(wrong_command);
+	aura::parser::specific_source parser_ut(specific_source);
 	const auto parse_result = parser_ut.parse(test_string_view);
 	EXPECT_FALSE(parse_result);
 }
