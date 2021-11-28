0479d16bda5639565b0d6e9491bed8630f7b29ff
Rafal <komarro@gmail.com>
2021-11-28 18:02:28 +0100

    ut version added

---
+++ lib/auralib/parsers/commands/version.cpp
@@ -0,0 +1,60 @@
+/*
+ * verion.cpp
+ */
+
+#include "version.hpp"
+
+namespace aura
+{
+
+namespace parser
+{
+
+namespace commands
+{
+
+const std::string& version_pcode::get_token() const
+{
+	return PCODE_TOKEN;
+}
+
+bool version_pcode::is_value_ok(const std::string_view& value) const
+{
+	return (value.size() != 0);
+}
+
+const std::string& version_fw_version::get_token() const
+{
+	return FW_VERSION_TOKEN;
+}
+
+bool version_fw_version::is_value_ok(const std::string_view& value) const
+{
+	return (value.size() != 0);
+}
+
+const std::string& version_hw_version::get_token() const
+{
+	return HW_VERSION_TOKEN;
+}
+
+bool version_hw_version::is_value_ok(const std::string_view& value) const
+{
+	return (value.size() != 0);
+}
+
+const std::string& version_man_code::get_token() const
+{
+	return MAN_CODE_TOKEN;
+}
+
+bool version_man_code::is_value_ok(const std::string_view& value) const
+{
+	return (value.size() != 0);
+}
+
+}
+
+}
+
+}
---
+++ lib/auralib/parsers/commands/version.hpp
@@ -0,0 +1,72 @@
+/*
+ * version.hpp
+ */
+
+#ifndef COMMAND_VERSION_HPP_
+#define COMMAND_VERSION_HPP_
+
+#include "simple_value.hpp"
+
+/*
+PCODE: 300c
+FVER: 2.4
+HVER: 1.0
+MANCODE: 30
+ */
+
+namespace aura
+{
+
+namespace parser
+{
+
+namespace commands
+{
+
+class version_pcode : public simple_value
+{
+public:
+	virtual const std::string& get_token() const;
+	virtual bool is_value_ok(const std::string_view& value) const;
+
+private:
+	const std::string PCODE_TOKEN = "PCODE: ";
+};
+
+class version_fw_version : public simple_value
+{
+public:
+	virtual const std::string& get_token() const;
+	virtual bool is_value_ok(const std::string_view& value) const;
+
+private:
+	const std::string FW_VERSION_TOKEN = "FVER: ";
+};
+
+class version_hw_version : public simple_value
+{
+public:
+	virtual const std::string& get_token() const;
+	virtual bool is_value_ok(const std::string_view& value) const;
+
+private:
+	const std::string HW_VERSION_TOKEN = "HVER: ";
+};
+
+class version_man_code : public simple_value
+{
+public:
+	virtual const std::string& get_token() const;
+	virtual bool is_value_ok(const std::string_view& value) const;
+
+private:
+	const std::string MAN_CODE_TOKEN = "MANCODE: ";
+};
+
+}
+
+}
+
+}
+
+#endif
