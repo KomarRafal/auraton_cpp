1089c9f643c6b45ab172fe502146f219b4b3b20d
Rafal <komarro@gmail.com>
2021-12-05 17:35:32 +0100

    set xtal correction

--- lib/auralib/parsers/commands/xtal_correction.cpp
+++ lib/auralib/parsers/commands/xtal_correction.cpp
@@ -19,11 +19,17 @@ namespace parser
 namespace commands
 {
 
-const std::string xtal_correction::COMMAND_XTAL_CORRECTION_TOKEN = "CRYSTALCORRECTION?";
+const std::string xtal_correction::COMMAND_SET_XTAL_CORRECTION_TOKEN = "CRYSTALCORRECTION";
+const std::string xtal_correction::COMMAND_GET_XTAL_CORRECTION_TOKEN = xtal_correction::COMMAND_SET_XTAL_CORRECTION_TOKEN + "?";
 
 const std::string xtal_correction::get_command_token()
 {
-	return COMMAND_XTAL_CORRECTION_TOKEN;
+	return COMMAND_GET_XTAL_CORRECTION_TOKEN;
+}
+
+const std::string xtal_correction::get_set_command_token()
+{
+	return COMMAND_SET_XTAL_CORRECTION_TOKEN;
 }
 
 const std::string& xtal_correction::get_token() const
@@ -42,7 +48,16 @@ bool xtal_correction::is_value_ok(const std::string_view& value) const
 	return false;
 }
 
-parser_if::parser_algorithm_t xtal_correction_builder::build()
+/*
+AT+CRYSTALCORRECTION?
+AT:START
+SOURCE:COMMAND
+COMMAND: CRYSTALCORRECTION?
+STATUS:OK
+VALUE: -70000
+AT:STOP
+*/
+parser_if::parser_algorithm_t get_xtal_correction_builder::build()
 {
 	parser_if::parser_ptr at_parser = std::make_unique<at>();
 	parser_if::parser_ptr source_commnand_parser = std::make_unique<specific_source>(command::SOURCE_TOKEN);
@@ -56,6 +71,29 @@ parser_if::parser_algorithm_t xtal_correction_builder::build()
 	parse_algorithm.push_back(std::move(test_command_parser));
 	parse_algorithm.push_back(std::move(status_parser));
 	parse_algorithm.push_back(std::move(xtal_correction_parser));
+	return parse_algorithm;
+}
+
+/*
+AT+CRYSTALCORRECTION=-71000
+AT:START
+SOURCE:COMMAND
+COMMAND: CRYSTALCORRECTION
+STATUS:OK
+AT:STOP
+*/
+parser_if::parser_algorithm_t set_xtal_correction_builder::build()
+{
+	parser_if::parser_ptr at_parser = std::make_unique<at>();
+	parser_if::parser_ptr source_commnand_parser = std::make_unique<specific_source>(command::SOURCE_TOKEN);
+	parser_if::parser_ptr test_command_parser = std::make_unique<specific_command>(xtal_correction::get_set_command_token());
+	parser_if::parser_ptr status_parser = std::make_unique<status>();
+
+	parser_if::parser_algorithm_t parse_algorithm;
+	parse_algorithm.push_back(std::move(at_parser));
+	parse_algorithm.push_back(std::move(source_commnand_parser));
+	parse_algorithm.push_back(std::move(test_command_parser));
+	parse_algorithm.push_back(std::move(status_parser));
 	return parse_algorithm;
 }
 
